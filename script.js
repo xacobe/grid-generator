aa8052ae6e390650e3520949dc515c12853d4026
xacobe <xacobe.net@gmail.com>
2015-02-25 18:42:43 +0100

    Second commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
   
   /* Close parent div*/
@@ -11,14 +10,14 @@ $(document).ready(function(){
     **/
 
   /* Add main grid*/
-    $(".js-create-main-grid").click(function(){
-      $("main").append("<article class='g-1 l-box'><a class='js-create-grid' href='#'>Add</a>Some loremp ipsum factum </article>");
+    $(".js-create-grid").click(function(){
+      $(this).append('<article class="g-1 l-box"><a class="js-create-main-grid" href="#">Add</a></br>Some loremp ipsum factum <ul class="menu menu--set-grid"><li class="is-mb"><button>g-</button><ul class="submenu"><li><a class="js-set-1-1" href="#">g-1-1</a></li><li><a class="js-set-1-2" href="#">g-1-2</a></li><li><a class="js-set-1-3" href="#">g-1-3</a></li><li><a class="js-set-1-4" href="#">g-1-4</a></li><li><a class="js-set-1-5" href="#">g-1-5</a></li><li><a class="js-set-2-3" href="#">g-2-3</a></li></ul></article>');
   });     
     
     /* Add grid */
-  $(".js-create-grid").click(function(){
-      $(this).parent().append("<article class='g-1 l-box'><a class='js-create-grid' href='#'>Add</a>Some loremp ipsum factum </article>");
-  }); 
+  $(".js-create-main-grid").click(function(){
+          $("article").parent().append('<article class="g-1-1 l-box"><nav id="nav" role="navigation"><a class="js-close">x</a><a class="js-create-main-grid">+</a><ul class="menu menu--set-grid"><li class="is-mb"><button>g-</button><ul class="submenu"><li><a class="js-set-1-1" href="#">g-1-1</a></li><li><a class="js-set-1-2" href="#">g-1-2</a></li><li><a class="js-set-1-3" href="#">g-1-3</a></li><li><a class="js-set-1-4" href="#">g-1-4</a></li><li><a class="js-set-1-5" href="#">g-1-5</a></li><li><a class="js-set-2-3" href="#">g-2-3</a></li><li><a class="js-set-2-4" href="#">g-2-4</a></li><li><a class="js-set-2-5" href="#">g-2-5</a></li><li><a class="js-set-3-4" href="#">g-3-4</a></li><li><a class="js-set-4-5" href="#">g-4-5</a></li></ul>1-1</li><li class="is-sm"> <button>g-sm-</button> <ul class="submenu">  <li><a class="js-set-sm-1-1" href="#">g-sm-1-1</a></li>  <li><a class="js-set-sm-1-2" href="#">g-sm-1-2</a></li>  <li><a class="js-set-sm-1-3" href="#">g-sm-1-3</a></li>  <li><a class="js-set-sm-1-4" href="#">g-sm-1-4</a></li>  <li><a class="js-set-sm-1-5" href="#">g-sm-1-5</a></li> </ul> </li> <li class="is-md"> <button>g-md-</button> <ul class="submenu">  <li><a class="js-set-md-1-1" href="#">g-md-1-1</a></li>  <li><a class="js-set-md-1-2" href="#">g-md-1-2</a></li>  <li><a class="js-set-md-1-3" href="#">g-md-1-3</a></li>  <li><a class="js-set-md-1-4" href="#">g-md-1-4</a></li>  <li><a class="js-set-md-1-5" href="#">g-md-1-5</a></li> </ul> </li> <li class="is-lg"> <button>g-lg-</button> <ul class="submenu">  <li><a class="js-set-lg-1-1" href="#">g-lg-1-1</a></li>  <li><a class="js-set-lg-1-2" href="#">g-lg-1-2</a></li>  <li><a class="js-set-lg-1-3" href="#">g-lg-1-3</a></li>  <li><a class="js-set-lg-1-4" href="#">g-lg-1-4</a></li>  <li><a class="js-set-lg-1-5" href="#">g-lg-1-5</a></li> </ul> </li> <li> <button>g-xl-</button> <ul class="submenu">  <li><a class="js-set-xl-1-1" href="#">g-xl-1-1</a></li>  <li><a class="js-set-xl-1-2" href="#">g-xl-1-2</a></li>  <li><a class="js-set-xl-1-3" href="#">g-xl-1-3</a></li>  <li><a class="js-set-xl-1-4" href="#">g-xl-1-4</a></li>  <li><a class="js-set-xl-1-5" href="#">g-xl-1-5</a></li> </ul> </li> </ul> </nav> </article>');
+      }); 
 
   /* Set and remove grid classes */
 
